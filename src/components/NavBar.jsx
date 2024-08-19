160f2f94167a40c0106db8d7b88c5ff48f1fa9c8
nahidraz23 <nzamanraz@gmail.com>
2024-08-19 16:12:54 +0600

    navbar added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+
+    const navLinks = (
+        <div className='flex gap-4'>
+            <NavLink>Home</NavLink>
+            <NavLink>Event Details</NavLink>
+        </div>
+    )
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -22,32 +30,18 @@ const NavBar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {
+                            navLinks
+                        }
                     </ul>
                 </div>
                 <a className="btn btn-ghost text-xl">Organize Me</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {
+                        navLinks
+                    }
                 </ul>
             </div>
             <div className="navbar-end">
